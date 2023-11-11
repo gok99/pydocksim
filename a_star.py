4a36662241f59d90457dc7fedbe9a7a98345f610
thojianxiong <66673507+thojianxiong@users.noreply.github.com>
2023-11-12 00:06:07 +0800

    Add files via upload

---
+++ a_star.py
@@ -0,0 +1,252 @@
+import math
+from enum import Enum
+from dataclasses import dataclass
+from visualiser import visualise_game_state
+from visualiser import Action
+from queue import PriorityQueue
+
+# actions: 
+# F = forward L meters
+# B = backward L meters
+# R = move right L meters
+# L = move left L meters
+# Encircle(p, d) = encircle point p in d direction
+
+class Direction(Enum):
+    CW = 0
+    CCW = 1
+
+# The task of the USV is to (Assume animal positions are known and given): 
+# 1) encircle the platypus (P) CW
+# 2) encircle the turtle (T) CCW
+# 3) remain 10m away from the crocodile (C)
+
+@dataclass
+class GameState:
+    start_pos: tuple   # (x, y) start position of USV
+    usv_dims: tuple    # (width, height) of USV
+    P: tuple           # (x, y) position of platypus
+    T: tuple           # (x, y) position of turtle    
+    C: tuple           # (x, y) position of crocodile
+    grid_length: int   # length of grid
+    move_time: float   # time to move 1 grid length
+    circle_time: float # time to encircle 1m
+    time_limit: float  # time limit for episode
+
+@dataclass
+class State:
+    pos: tuple       # (x, y) position of USV
+    P: bool          # True if encircled
+    T: bool          # True if encircled
+    C: bool          # True if > 10m away so far
+    time: float      # time elapsed so far
+
+    def __lt__(self, other):
+        # Define how States are compared based on their f-score
+        return self.time < other.time
+
+# coord convention:
+#  y
+#  |
+#  ---> x
+
+
+def pos_change(action: Action, game_state: GameState):
+    if action == Action.F:
+        return (0, game_state.grid_length)
+    elif action == Action.B:
+        return (0, -game_state.grid_length)
+    elif action == Action.R:
+        return (game_state.grid_length, 0)
+    elif action == Action.L:
+        return (-game_state.grid_length, 0)
+    else:
+        return (0, 0)
+
+def run_action(usv_state: State, game_state: GameState, action: Action, pc: tuple = None, dir: Direction = None):
+    new_usv_state = State(
+        pos = usv_state.pos,
+        P = usv_state.P,
+        T = usv_state.T,
+        C = usv_state.C,
+        time = usv_state.time
+    )
+
+    if action == Action.Encircle:
+        assert pc is not None and dir is not None, "Must specify pc and dir for Encircle action"
+
+        if dir == Direction.CW:
+            new_usv_state.P = True
+
+        elif dir == Direction.CCW:
+            new_usv_state.T = True
+
+        twopiR = 2 * math.pi * math.sqrt((usv_state.pos[0] - pc[0])**2 + (usv_state.pos[1] - pc[1])**2)
+        new_usv_state.time += twopiR * game_state.circle_time
+    
+    else:
+        new_usv_state.time += 1
+        if action == Action.F:
+            new_usv_state.pos = (usv_state.pos[0], usv_state.pos[1] + game_state.grid_length)
+        elif action == Action.B:
+            new_usv_state.pos = (usv_state.pos[0], usv_state.pos[1] - game_state.grid_length)
+        elif action == Action.R:
+            new_usv_state.pos = (usv_state.pos[0] + game_state.grid_length, usv_state.pos[1])
+        elif action == Action.L:
+            new_usv_state.pos = (usv_state.pos[0] - game_state.grid_length, usv_state.pos[1])
+
+    return new_usv_state
+
+def a_star_search(game_state: GameState, start_state: State, target: tuple):
+
+    def heuristic(pos, target):
+        return abs(pos[0] - target[0]) + abs(pos[1] - target[1])
+
+    def croc_obstacles (game_state: GameState):
+    # 10m euclidean radius circle around crocodile
+        obstacles = []
+        for i in range(game_state.C[0]-10, game_state.C[0]+10):
+            for j in range(game_state.C[1]-10, game_state.C[1]+10):
+                if (i-game_state.C[0])**2 + (j-game_state.C[1])**2 <= 100:
+                    obstacles.append((i,j))
+        return obstacles 
+    
+    croc_obstacles = croc_obstacles(game_state)
+    
+    def neighbours(state: State):
+        movements = [(Action.F, (state.pos[0], state.pos[1] + game_state.grid_length)),
+                     (Action.B, (state.pos[0], state.pos[1] - game_state.grid_length)), 
+                     (Action.R, (state.pos[0] + game_state.grid_length, state.pos[1])), 
+                     (Action.L, (state.pos[0] - game_state.grid_length, state.pos[1]))]
+        valid_movements = []
+        for action, new_pos in movements:
+            if new_pos in croc_obstacles:
+                continue
+            else:
+                valid_movements.append((action, new_pos))
+        return valid_movements
+    
+    def reconstruct_path(came_from, current_state, parent_act):
+        total_path = [parent_act]
+        while current_state.pos in came_from:
+            current_state, parent_act = came_from[current_state.pos]
+            total_path.append(parent_act)
+        return total_path[::-1]
+    
+    open_set = PriorityQueue() ## (f_score, state, action_from_parent)
+    open_set.put((0, start_state, None))
+    came_from = {}
+
+    g_score = {start_state.pos: 0}
+    f_score = {start_state.pos: heuristic(start_state.pos, target)}
+
+    while not open_set.empty():
+        curr_f_score, current_state, parent_act = open_set.get()
+
+        if current_state.pos == target:
+            path = reconstruct_path(came_from, current_state, parent_act)
+            return path
+
+        for action, neighbor_pos in neighbours(current_state):
+            neighbor_state = State(
+                pos=neighbor_pos,
+                P=current_state.P,
+                T=current_state.T,
+                C=current_state.C,
+                time=current_state.time + 1
+            )
+
+            tentative_g_score = g_score[current_state.pos] + 1
+            if neighbor_state.pos in g_score and tentative_g_score >= g_score[neighbor_state.pos]:
+                continue
+
+            came_from[neighbor_state.pos] = current_state, action
+            g_score[neighbor_state.pos] = tentative_g_score
+            f_score[neighbor_state.pos] = tentative_g_score + heuristic(neighbor_pos, target)
+
+            open_set.put((f_score[neighbor_state.pos], neighbor_state, action))
+
+    return None  # No path found
+
+    
+
+def main():
+    game_state = GameState(
+        start_pos = (0, 0),
+        usv_dims = (1, 1),
+        P = (39, 39),
+        T = (10, 10),
+        C = (30, 30),
+        grid_length = 1,
+        time_limit = 100,
+        move_time = 1,
+        circle_time = 1
+    )
+    state = State(
+        pos=game_state.start_pos,
+        P=False,
+        T=False,
+        C=True,
+        time=0
+    )
+    ## determine a grid point that is less than 3 meters away from platypus or turtle
+    ## A* search a path to that point and then encircle platypus or turtle, whichever is closer
+    ## Then repeat A* again for the other animal
+    ## Treat the radius 10m of the crocodile as obstacles
+
+
+    animals_to_encircle = [(game_state.P,"platypus"), (game_state.T,"turtle")]
+    action_history = []
+    time = 0
+
+    while not (state.P and state.T):
+        if len(animals_to_encircle) == 0:
+            break
+        possible_targets = []
+        for animal in animals_to_encircle:
+            possible_targets.append(((animal[0][0]+3, animal[0][1]), animal[1]))
+            possible_targets.append(((animal[0][0]-3, animal[0][1]), animal[1]))
+            possible_targets.append(((animal[0][0], animal[0][1]+3), animal[1]))
+            possible_targets.append(((animal[0][0], animal[0][1]-3), animal[1]))
+            print(possible_targets)
+
+        best = possible_targets[0]
+        
+        for target in possible_targets:
+            if abs(target[0][0]- game_state.start_pos[0]) + abs(target[0][1]- game_state.start_pos[1]) < abs(best[0][0]- game_state.start_pos[0]) + abs(best[0][1]- game_state.start_pos[1]):
+                best = target
+
+        path = a_star_search(game_state, state, best[0])
+
+        if not path:
+            print("No valid path found.")
+            return None
+        else:
+            if best[1] == "platypus":
+                animals_to_encircle.remove((game_state.P,"platypus"))
+                state.P = False
+            else:
+                animals_to_encircle.remove((game_state.T,"turtle"))
+                state.T = False
+        
+        for action in path:
+            state = run_action(state, game_state, action)
+            action_history.append(action)
+            time += 1
+            
+        if best[1] == "platypus":
+            action_history.append(Action.Encircle)
+            action_history.append(game_state.P)
+            time += 2 * math.pi * 3
+
+        else:
+            action_history.append(Action.Encircle)
+            action_history.append(game_state.T)
+            time += 2 * math.pi * 3
+
+    print(f"Total time taken: {time}")
+    visualise_game_state(game_state, action_history)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ classical.py
@@ -0,0 +1,252 @@
+import math
+from enum import Enum
+from dataclasses import dataclass
+from visualiser import visualise_game_state
+from visualiser import Action
+import random
+
+# actions: 
+# F = forward L meters
+# B = backward L meters
+# R = move right L meters
+# L = move left L meters
+# Encircle(p, d) = encircle point p in d direction
+
+class Direction(Enum):
+    CW = 0
+    CCW = 1
+
+# The task of the USV is to (Assume animal positions are known and given): 
+# 1) encircle the platypus (P) CW
+# 2) encircle the turtle (T) CCW
+# 3) remain 10m away from the crocodile (C)
+
+@dataclass
+class GameState:
+    start_pos: tuple   # (x, y) start position of USV
+    usv_dims: tuple    # (width, height) of USV
+    P: tuple           # (x, y) position of platypus
+    T: tuple           # (x, y) position of turtle    
+    C: tuple           # (x, y) position of crocodile
+    grid_length: int   # length of grid
+    move_time: float   # time to move 1 grid length
+    circle_time: float # time to encircle 1m
+    time_limit: float  # time limit for episode
+
+@dataclass
+class State:
+    pos: tuple       # (x, y) position of USV
+    P: bool          # True if encircled
+    T: bool          # True if encircled
+    C: bool          # True if > 10m away so far
+    time: float      # time elapsed so far
+
+# rewards:
+# -1 for each time step
+# +30 for encircling platypus (if not collided with animal)
+# +30 for encircling turtle   (if not collided with animal)
+# +30 for staying > 10m away from crocodile (at the end of the episode)
+
+def reward(usv_state: State, game_state: GameState):
+    reward = 0
+    reward -= usv_state.time
+    
+    if usv_state.P:
+        reward += 30
+    if usv_state.T:
+        reward += 30
+    
+    time_exceeded = usv_state.time >= game_state.time_limit
+    game_over = (usv_state.P and usv_state.T) or time_exceeded
+    
+    if game_over and usv_state.C:
+            reward += 30
+    
+    return reward
+
+# coord convention:
+#  y
+#  |
+#  ---> x
+
+def will_collide(usv_state: State, game_state: GameState, action: Action, pc: tuple = None, dir: Direction = None):
+    action = determine_action(usv_state.pos, game_state.P, game_state.T, game_state.C, usv_state.P, usv_state.T)
+    pos = usv_state.pos
+    if action == Action.F:
+        pos = (pos[0], pos[1] + game_state.grid_length)
+    elif action == Action.B:
+        pos = (pos[0], pos[1] - game_state.grid_length)
+    elif action == Action.R:
+        pos = (pos[0] + game_state.grid_length, pos[1])
+    elif action == Action.L:
+        pos = (pos[0] - game_state.grid_length, pos[1])
+    croc_pos = game_state.C
+    print(pos, croc_pos)
+    if abs(pos[0]-croc_pos[0]) + abs(pos[1]-croc_pos[1]) <= 10:
+        return True
+
+def position_encircled(usv_pos: tuple, circle_pos: tuple, test_pos: tuple):
+    radius = math.sqrt((usv_pos[0] - circle_pos[0])**2 + (usv_pos[1] - circle_pos[1])**2)
+    return math.sqrt((test_pos[0] - circle_pos[0])**2 + (test_pos[1] - circle_pos[1])**2) <= radius
+
+def run_action(usv_state: State, game_state: GameState, action: Action, pc: tuple = None, dir: Direction = None):
+    new_usv_state = State(
+        pos = usv_state.pos,
+        P = usv_state.P,
+        T = usv_state.T,
+        C = usv_state.C,
+        time = usv_state.time
+    )
+    
+    if will_collide(usv_state, game_state, action):
+        return None, usv_state
+
+    if action == Action.Encircle:
+        assert pc is not None and dir is not None, "Must specify pc and dir for Encircle action"
+
+        if dir == Direction.CW:
+            plat_circled = position_encircled(usv_state.pos, pc, game_state.P)
+            new_usv_state.P = new_usv_state.P or plat_circled
+
+        elif dir == Direction.CCW:
+            turtle_circled = position_encircled(usv_state.pos, pc, game_state.T)
+            new_usv_state.T = new_usv_state.T or turtle_circled
+
+        twopiR = 2 * math.pi * math.sqrt((usv_state.pos[0] - pc[0])**2 + (usv_state.pos[1] - pc[1])**2)
+        new_usv_state.time += twopiR * game_state.circle_time
+    
+    # update position
+    else:
+        new_usv_state.time += 1
+        if action == Action.F:
+            new_usv_state.pos = (usv_state.pos[0], usv_state.pos[1] + game_state.grid_length)
+        elif action == Action.B:
+            new_usv_state.pos = (usv_state.pos[0], usv_state.pos[1] - game_state.grid_length)
+        elif action == Action.R:
+            new_usv_state.pos = (usv_state.pos[0] + game_state.grid_length, usv_state.pos[1])
+        elif action == Action.L:
+            new_usv_state.pos = (usv_state.pos[0] - game_state.grid_length, usv_state.pos[1])
+
+    return action, new_usv_state
+
+def determine_action(usv_pos, platypus_pos, turtle_pos, crocodile_pos, platypus_encircled, turtle_encircled):
+
+    if random.random() < 0.2:
+            return random.choice([Action.F, Action.L, Action.B, Action.R]), None, None
+    
+    def calculate_distance(pos1, pos2):
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+    usv_to_platypus_distance = calculate_distance(usv_pos, platypus_pos)
+    usv_to_turtle_distance = calculate_distance(usv_pos, turtle_pos)
+    usv_to_crocodile_distance = calculate_distance(usv_pos, crocodile_pos)
+
+    if usv_to_crocodile_distance <= 10:
+        # Move away from the crocodile
+        if crocodile_pos[1] > usv_pos[1]:
+            return Action.B, None, None
+        elif crocodile_pos[1] < usv_pos[1]:
+            return Action.F, None, None
+        elif crocodile_pos[0] > usv_pos[0]:
+            return Action.L, None, None
+        else:
+            return Action.R, None, None
+
+    if not platypus_encircled:
+        if usv_to_platypus_distance < 3:
+            return Action.Encircle, platypus_pos, Direction.CW
+    if not turtle_encircled:
+        if usv_to_turtle_distance < 3:
+            return Action.Encircle, turtle_pos, Direction.CCW
+        
+    if not platypus_encircled and not turtle_encircled:   
+        if usv_to_platypus_distance < usv_to_turtle_distance:
+            if platypus_pos[1] > usv_pos[1]:
+                return Action.F, None, None
+            elif platypus_pos[1] < usv_pos[1]:
+                return Action.B, None, None
+            elif platypus_pos[0] > usv_pos[0]:
+                return Action.R, None, None
+            else:
+                return Action.L, None, None
+        else:
+            if turtle_pos[1] > usv_pos[1]:
+                return Action.F, None, None
+            elif turtle_pos[1] < usv_pos[1]:
+                return Action.B, None, None
+            elif turtle_pos[0] > usv_pos[0]:
+                return Action.R, None, None
+            else:
+                return Action.L, None, None
+    elif not platypus_encircled:
+        if platypus_pos[1] > usv_pos[1]:
+            return Action.F, None, None
+        elif platypus_pos[1] < usv_pos[1]:
+            return Action.B, None, None
+        elif platypus_pos[0] > usv_pos[0]:
+            return Action.R, None, None
+        else:
+            return Action.L, None, None
+    elif not turtle_encircled:
+        if turtle_pos[1] > usv_pos[1]:
+            return Action.F, None, None
+        elif turtle_pos[1] < usv_pos[1]:
+            return Action.B, None, None
+        elif turtle_pos[0] > usv_pos[0]:
+            return Action.R, None, None
+        else:
+            return Action.L, None, None
+    else:
+        # If both animals are encircled, move forward
+        return None, None, None
+
+def main():
+    game_state = GameState(
+        start_pos = (0, 0),
+        usv_dims = (1, 1),
+        P = (45, 45),
+        T = (10, 10),
+        C = (30, 30),
+        grid_length = 1,
+        time_limit = 200,
+        move_time = 1,
+        circle_time = 1
+    )
+    state = State(
+        pos=game_state.start_pos,
+        P=False,
+        T=False,
+        C=True,
+        time=0
+    )
+    action_history = []
+    curr_state = state
+    
+    while not (state.P and state.T):
+        print(curr_state)
+        
+        if curr_state.time > game_state.time_limit:
+            break
+        action, pc, dir = determine_action(curr_state.pos, game_state.P, game_state.T, game_state.C, state.P, state.T)
+        if action == None:
+            break
+        take_action, curr_state = run_action(curr_state, game_state, action, pc, dir)
+        if take_action == None:
+            curr_state.time +=1
+            continue
+        action_history.append(action)
+        print(action)
+        if action == Action.Encircle:
+            if dir == Direction.CW:
+                state.P = True
+                action_history.append(game_state.P)
+
+            elif dir == Direction.CCW:
+                state.T = True
+                action_history.append(game_state.T)
+
+    print(f"Total time taken: {curr_state.time}")
+    visualise_game_state(game_state, action_history)
+    
+
+if __name__ == "__main__":
+    main()
